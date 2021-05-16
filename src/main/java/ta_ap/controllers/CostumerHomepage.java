1102731345bcfb2376ac9608e910565768eeee26
aivalfmioc <flaviamioc@yahoo.com>
2021-05-16 22:46:36 +0300

    TA-55 Costumer has now a homepage. He can choose to view accomodations or to make reservation

---
+++ src/main/java/ta_ap/controllers/CostumerHomepage.java
@@ -0,0 +1,24 @@
+package ta_ap.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class CostumerHomepage {
+    public Button button;
+    private FXMLLoader scene;
+
+    public void gotoViewAcom(ActionEvent actionEvent) throws IOException {
+        button.getScene().setRoot(FXMLLoader.load(getClass().getResource("/costumer.fxml")));
+    }
+
+    public FXMLLoader getScene() {
+        return scene;
+    }
+
+    public void setScene(FXMLLoader scene) {
+        this.scene = scene;
+    }
+}
